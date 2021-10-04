a2161be53869f1e520f93b9db80868f9335e9987
Gutierre <guti_thiago@hotmail.com>
2021-10-04 13:50:14 -0300

    Feat?: configurando navigation.xml

---
+++ app/src/main/java/com/example/coinconverterspeech/presentation/CoinConverterFragment.kt
@@ -0,0 +1,104 @@
+package com.example.coinconverterspeech.presentation
+
+import android.os.Bundle
+import android.view.*
+import android.widget.ArrayAdapter
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import com.example.coinconverterspeech.core.extensions.*
+import com.example.coinconverterspeech.data.model.Coin
+import com.example.coinconverterspeech.databinding.FragmentCoinConverterBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class CoinConverterFragment: Fragment() {
+
+
+    private val viewModel by viewModel<MainViewModel>()
+    private val dialog by lazy { requireContext().createProgressDialog() }
+    private val binding by lazy { FragmentCoinConverterBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bindAdapters()
+        bindListeners()
+        bindObserve()
+
+    }
+
+
+    private fun bindAdapters() {
+        val list = Coin.values()
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, list)
+
+        binding.tvFrom.setAdapter(adapter)
+        binding.tvTo.setAdapter(adapter)
+
+        binding.tvFrom.setText(Coin.USD.name, false)
+        binding.tvTo.setText(Coin.BRL.name, false)
+    }
+
+    private fun bindListeners() {
+        binding.tilValue.editText?.doAfterTextChanged {
+            binding.btnConverter.isEnabled = it != null && it.toString().isNotEmpty()
+            binding.btnSave.isEnabled = false
+        }
+
+        binding.btnConverter.setOnClickListener {
+            it.hideSoftKeyboard()
+
+            val search = "${binding.tilFrom.text}-${binding.tilTo.text}"
+
+            viewModel.getExchangeValue(search)
+        }
+
+        binding.btnSave.setOnClickListener {
+            val value = viewModel.state.value
+            (value as? MainViewModel.State.Success)?.let {
+                val exchange = it.exchange.copy(bid = it.exchange.bid * binding.tilValue.text.toDouble())
+                viewModel.saveExchange(exchange)
+            }
+        }
+    }
+
+    private fun bindObserve() {
+        viewModel.state.observe(viewLifecycleOwner) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    dialog.dismiss()
+                    requireContext().createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                }
+                is MainViewModel.State.Success -> success(it)
+                MainViewModel.State.Saved -> {
+                    dialog.dismiss()
+                    requireContext().createDialog {
+                        setMessage("item salvo com sucesso!")
+                    }.show()
+                }
+            }
+        }
+    }
+
+    private fun success(it: MainViewModel.State.Success) {
+        dialog.dismiss()
+        binding.btnSave.isEnabled = true
+
+        val selectedCoin = binding.tilTo.text
+        val coin = Coin.getByName(selectedCoin)
+
+        val result = it.exchange.bid * binding.tilValue.text.toDouble()
+
+        binding.tvResult.text = result.formatCurrency(coin.locale)
+    }
+}
--- app/src/main/java/com/example/coinconverterspeech/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/coinconverterspeech/ui/main/MainActivity.kt
@@ -4,39 +4,35 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doAfterTextChanged
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.coinconverterspeech.R
-import com.example.coinconverterspeech.core.extensions.createDialog
-import com.example.coinconverterspeech.core.extensions.createProgressDialog
-import com.example.coinconverterspeech.core.extensions.formatCurrency
-import com.example.coinconverterspeech.core.extensions.hideSoftKeyboard
-import com.example.coinconverterspeech.core.extensions.text
-import com.example.coinconverterspeech.data.model.Coin
 import com.example.coinconverterspeech.databinding.ActivityMainBinding
-import com.example.coinconverterspeech.presentation.MainViewModel
 import com.example.coinconverterspeech.ui.history.HistoryActivity
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel by viewModel<MainViewModel>()
-    private val dialog by lazy { createProgressDialog() }
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    lateinit var host: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
 
-        bindAdapters()
-        bindListeners()
-        bindObserve()
+
+        setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
+        host = NavHostFragment.create(R.navigation.navigation)
+        setNavControler()
+        setBottomNavigationMenu()
 
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
@@ -50,70 +46,12 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun bindAdapters() {
-        val list = Coin.values()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
-
-        binding.tvFrom.setAdapter(adapter)
-        binding.tvTo.setAdapter(adapter)
-
-        binding.tvFrom.setText(Coin.USD.name, false)
-        binding.tvTo.setText(Coin.BRL.name, false)
+    private fun setBottomNavigationMenu() {
+        binding.navigationBottom.setupWithNavController(findNavController(R.id.fragment_main))
     }
 
-    private fun bindListeners() {
-        binding.tilValue.editText?.doAfterTextChanged {
-            binding.btnConverter.isEnabled = it != null && it.toString().isNotEmpty()
-            binding.btnSave.isEnabled = false
-        }
-
-        binding.btnConverter.setOnClickListener {
-            it.hideSoftKeyboard()
-
-            val search = "${binding.tilFrom.text}-${binding.tilTo.text}"
-
-            viewModel.getExchangeValue(search)
-        }
-
-        binding.btnSave.setOnClickListener {
-            val value = viewModel.state.value
-            (value as? MainViewModel.State.Success)?.let {
-                val exchange = it.exchange.copy(bid = it.exchange.bid * binding.tilValue.text.toDouble())
-                viewModel.saveExchange(exchange)
-            }
-        }
+    fun setNavControler(){
+        navController = findNavController(R.id.fragment_main)
     }
 
-    private fun bindObserve() {
-        viewModel.state.observe(this) {
-            when (it) {
-                MainViewModel.State.Loading -> dialog.show()
-                is MainViewModel.State.Error -> {
-                    dialog.dismiss()
-                    createDialog {
-                        setMessage(it.error.message)
-                    }.show()
-                }
-                is MainViewModel.State.Success -> success(it)
-                MainViewModel.State.Saved -> {
-                    dialog.dismiss()
-                    createDialog {
-                        setMessage("item salvo com sucesso!")
-                    }.show()
-                }
-            }
-        }
-    }
-
-    private fun success(it: MainViewModel.State.Success) {
-        dialog.dismiss()
-        binding.btnSave.isEnabled = true
-
-        val selectedCoin = binding.tilTo.text
-        val coin = Coin.getByName(selectedCoin)
-
-        val result = it.exchange.bid * binding.tilValue.text.toDouble()
-
-        binding.tvResult.text = result.formatCurrency(coin.locale)
-    }
 }
